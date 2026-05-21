00c4d880944867ae71819f30eb6112aa992fb311
Hatsune Miku <3022718261@qq.com>
2026-05-22 00:46:28 +0800

    add code

--- book.c
+++ book.c
@@ -372,8 +372,8 @@ void book_print(BookNode *book)
     printf("  ║  已借出:   %-34d ║\n", b->borrowed_count);
     printf("  ║  在馆数:   %-34d ║\n", book_available(book));
     printf("  ╚══════════════════════════════════════════════╝\n");
+    book_print_authors(b->authors);
 }
-
 int book_available(BookNode *book)
 {
     return book->info.total_copies - book->info.borrowed_count;
